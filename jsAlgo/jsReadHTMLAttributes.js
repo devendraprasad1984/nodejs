35674652922c0abb7cc54ddd41f5fe367940fe97
devendraprasad1984@gmail.com <devendraprasad1984@gmail.com>
2020-04-11 14:53:00 +0530

    how js and attribute works

--- jsAlgo/jsReadHTMLAttributes.js
+++ jsAlgo/jsReadHTMLAttributes.js
@@ -1,12 +1,20 @@
 //js to read html attribues
 let gcontext={};
 window.processData=function(p1,p2){
-    let {id,name,data,updateWithData}=gcontext;
-    data=JSON.parse(data)['data'];
-    console.log('in processData',p1,p2,id,name,updateWithData,data);
+    let {id,name,data,updateWithData,replaceWithApi}=gcontext;
     if(updateWithData==='true'){
         let oldVal=document.getElementById(id).innerHTML+`<br/>`;
-        document.getElementById(id).innerHTML=oldVal+ data.map(x=>`<li>${x}</li>`).join('');
+        data=JSON.parse(data)['data'];
+        console.log('in processData',p1,p2,id,name,updateWithData,data,replaceWithApi);
+        if(replaceWithApi!==null){
+            fetch(replaceWithApi).then((res)=>res.json()).then((res)=>{
+                console.log(res);
+                data=res;
+                document.getElementById(id).innerHTML=oldVal+ data.map(x=>`<li>${x.name} - ${x.email}</li>`).join('');
+            });
+        }else{
+            document.getElementById(id).innerHTML=oldVal+ data.map(x=>`<li>${x}</li>`).join('');
+        }
         // document.getElementById(id).innerHTML=data   ;
     }
 }
@@ -19,8 +27,9 @@ for(let i=0;i<tags.length;i++){
     let name=curTag.getAttribute('name');
     fnname=curTag.getAttribute('fn');
     let data=curTag.getAttribute('data');
+    let replaceWithApi=curTag.getAttribute('replaceWithApi');
     let updateWithData=curTag.getAttribute('updateWithData');
-    gcontext={id,name,data,updateWithData};
+    gcontext={id,name,data,updateWithData,replaceWithApi};
     if(fnname===null) continue;
     let fn2execute=new Function(fnname);
     fn2execute();
@@ -29,16 +38,16 @@ for(let i=0;i<tags.length;i++){
 }
 
 
-
-//everything is an object is js, eg class is also an object
-class X {
-    method1(){
-        console.log("1");
-    }
-    method2(){
-        this['method1']();
-        console.log("2");
-    }
-}
-let x  = new X();
-x['method2']();
+//
+// //everything is an object is js, eg class is also an object
+// class X {
+//     method1(){
+//         console.log("1");
+//     }
+//     method2(){
+//         this['method1']();
+//         console.log("2");
+//     }
+// }
+// let x  = new X();
+// x['method2']();
