505059e32a97be7bb51cb808cdc777ec78278dcf
Devendra Prasad <devendraprasad1984@gmail.com>
2021-05-15 16:09:59 +0530

    update

--- ts-node-postgres-orm-api/entity/post.ts
+++ ts-node-postgres-orm-api/entity/post.ts
@@ -1,1 +1,18 @@
-import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm'
+import {Entity, Column, PrimaryGeneratedColumn, ManyToMany, JoinTable} from 'typeorm'
+import {Category} from "./category";
+
+@Entity()
+export class Post{
+    @PrimaryGeneratedColumn()
+    id:number
+
+    @Column()
+    title: string
+
+    @Column('text')
+    text: string
+
+    @ManyToMany(type=>Category)
+    @JoinTable()
+    category: Category[]
+}
