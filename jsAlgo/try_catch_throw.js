ffb5c70b563e5edec5dc85002a8297b113a9a3c6
devendra prasad <devendraprasad1984@gmail.com>
2022-01-07 21:18:41 +0530

    updates

--- jsAlgo/try_catch_throw.js
+++ jsAlgo/try_catch_throw.js
@@ -1,8 +1,11 @@
+class InputError extends Error{}
+
 function promptDirection(question) {
     let result = prompt(question);
     if (result.toLowerCase() == "left") return "L";
     if (result.toLowerCase() == "right") return "R";
     throw new Error("Invalid direction: " + result);
+    throw new InputError("Invalid Input direction: " + result);
 }
 
 function look() {
@@ -17,4 +20,7 @@ try {
     console.log("You see", look());
 } catch (error) {
     console.log("Something went wrong: " + error);
+    if(error instanceof InputError){
+        console.log("InputError instance - Something went wrong: " + error);
+    }
 }
