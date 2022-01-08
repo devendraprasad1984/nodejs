639d63485c36a92172d8398802be4124780791b4
devendra prasad <devendraprasad1984@gmail.com>
2022-01-08 11:34:37 +0530

    updates

---
+++ jsAlgo/eventloop.js
@@ -0,0 +1,17 @@
+let start = Date.now();
+setTimeout(() => {
+    console.log("Timeout ran at", Date.now() - start);
+}, 20);
+while (Date.now() < start + 50) {
+}
+console.log("Wasted time until", Date.now() - start);
+// → Wasted time until 50
+// → Timeout ran at 55
+
+
+// Promises always resolve or reject as a new event. Even if a promise is already resolved, waiting for it will cause your callback to run after the current script finishes, rather than right away.
+//next console is printed first and promise console is printed later due to event loop
+Promise.resolve("Done").then(v=>console.log(v, 'now called'));
+console.log("Me first!");
+// → Me first!
+// → Done
