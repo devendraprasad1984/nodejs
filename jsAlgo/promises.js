b825098222d42617f13460c237650d04118bf10a
devendra prasad <devendraprasad1984@gmail.com>
2022-01-08 10:59:51 +0530

    updates

--- jsAlgo/promises.js
+++ jsAlgo/promises.js
@@ -9,3 +9,31 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
 let fifteen = Promise.resolve(15)
 fifteen.then(v => console.log('promise is resolved', v))
 
+function buttonClick() {
+    return new Promise((resolve, reject) => {
+        //only resolve or reject would be returned, any state is final fulfilled, error
+        let x = 0
+        x === 0 ? resolve('completed button click') : reject(new Error('failing with code'))
+        setTimeout(() => reject('settimeout error'), 2000) //will be ignore
+    })
+}
+
+let btn1 = buttonClick().then(success => {
+    console.log('promise success', success)
+}, error => {
+    console.log('promise error', error)
+}).catch(e => {
+    console.log('promise in catch error', e)
+}).finally(() => console.log('promise ends'))
+
+
+new Promise((_, reject) => {
+    return reject(new Error("failed rejection as returned"))
+})
+.then(value => console.log("Handler 1", value))
+.catch(reason => {
+    console.log("Caught failure " + reason);
+    return "nothing";
+})
+.then(value => console.log("Handler 2", value));
+
