beabdf09a4dfc3b4a4a62968abda715d775cdf64
devendra prasad <devendraprasad1984@gmail.com>
2022-02-15 13:39:56 +0530

    update

--- realtime_chat_socket.io_node/chat_server.js
+++ realtime_chat_socket.io_node/chat_server.js
@@ -4,12 +4,21 @@ const io = require('socket.io')(6201, {
     }
 })
 
+const users = {}
+
 io.on('connection', (socket) => {
     console.log('on connections server - new user')
     // socket.emit('chat-message', 'you are connected to chat server')
-
+    socket.on('new-user', user => {
+        users[socket.id] = user
+        socket.broadcast.emit('user-connected', user)
+    })
     socket.on('send-chat-message', (chat) => {
         console.log('from client', chat)
-        socket.broadcast.emit('chat-message', chat) //will send to every other client connect but to the one who sends
+        socket.broadcast.emit('chat-message', {chat: chat, name: users[socket.id]}) //will send to every other client connect but to the one who sends
+    })
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
     })
 })
--- realtime_chat_socket.io_node/script.js
+++ realtime_chat_socket.io_node/script.js
@@ -1,13 +1,34 @@
-const socket=window.io('http://localhost:6201') //this is come as part of socket.io exposing an io object in js
-const messageForm=document.getElementById('send-container')
-const messageInput=document.getElementById('message-input')
+const socket = window.io('http://localhost:6201') //this is come as part of socket.io exposing an io object in js
 
-socket.on('chat-message',(msg)=>{
-    console.log('on connections - new user')
-    console.log('receiving - ', msg)
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+const user = prompt('what is your name?')
+appendChat('Welcome, ' + user)
+socket.emit('new-user', user)
+
+socket.on('chat-message', (msg) => {
+    // console.log('on connections - new user')
+    // console.log('receiving - ', msg)
+    appendChat(`${msg.name}: ${msg.chat}`)
+})
+socket.on('user-connected', user => {
+    appendChat(`${user} user joined`)
+})
+socket.on('user-disconnected', user => {
+    appendChat(`${user} disconnected`)
 })
 
-messageForm.addEventListener('submit',e=>{
+messageForm.addEventListener('submit', e => {
     e.preventDefault()
-    socket.emit('send-chat-message',messageInput.value)
+    let msg = messageInput.value
+    appendChat(`you: ${msg}`)
+    socket.emit('send-chat-message', msg)
 })
+
+function appendChat(msg) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = msg
+    messageContainer.append(messageElement)
+}
