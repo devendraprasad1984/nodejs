802939221f913ac525829db38d522672bdc6775b
devendra prasad <devendraprasad1984@gmail.com>
2022-02-15 13:49:29 +0530

    update

--- npm_package_reuse/package/index.js
+++ npm_package_reuse/package/index.js
@@ -2,4 +2,11 @@ function dpfirst(str) {
     return str === "first"
 }
 
-module.exports = dpfirst
+// module.exports = dpfirst
+module.exports = {
+    dpfirst,
+    add: (a, b) => a + b,
+    sub: (a, b) => a - b,
+    mul: (a, b) => a * b,
+    div: (a, b) => a / b
+}
