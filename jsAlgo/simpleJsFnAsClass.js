adaf6646f484d70cd6c3ef1a412b3de21d33d1f5
devendra prasad <devendraprasad1984@gmail.com>
2022-03-06 17:39:44 +0530

    update

--- jsAlgo/simpleJsFnAsClass.js
+++ jsAlgo/simpleJsFnAsClass.js
@@ -13,3 +13,36 @@ let dp = new Person('devendra')
 let dp1 = new Person('akshat')
 dp.show()
 dp1.showMe()
+
+
+
+
+// class expression may/may not have a name
+let User = class {
+    sayHi() {
+        alert("Hello");
+    }
+};
+new User().sayHi();
+
+function makeClass(phrase) {
+    // declare a class and return it
+    return class {
+        sayHi() {
+            alert(phrase);
+        };
+    };
+}
+let User = makeClass("Hello");
+new User().sayHi(); // Hello
+
+
+// computed properties
+function f() { return "sayHi"; }
+class User {
+    [f()]() {
+        alert("Hello");
+    }
+
+}
+new User().sayHi();
