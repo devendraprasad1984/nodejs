2b172a43a51719ccad67a220f9ac3e31dc7e2321
devendra prasad <devendraprasad1984@gmail.com>
2022-03-13 21:23:45 +0530

    update

--- jsAlgo/pollyfill_reduce.js
+++ jsAlgo/pollyfill_reduce.js
@@ -3,23 +3,19 @@
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 Array.prototype.myReduce = function (fn, init, context) {
     let arr = this
-    let accumulator = init || undefined
+    let accumulator = init
     for (let i = 0; i < arr.length; i++) {
-        if (accumulator !== undefined) {
-            accumulator = fn.call(undefined, accumulator, arr[i])
-        } else {
-            accumulator = arr[0]
-        }
+        accumulator = fn(accumulator, arr[i])
     }
     return accumulator
 }
 
-let sum = arr.reduce((accumulator, index) => {
-    return accumulator + index
+let sum = arr.reduce((accumulator, num) => {
+    return accumulator + num
 }, 0)
 console.log('reduce built-in', sum)
 
-let sum1 = arr.myReduce((accumulator, index) => {
-    return accumulator + index
+let sum1 = arr.myReduce((accumulator, num) => {
+    return accumulator + num
 }, 0)
 console.log('reduce pollyfilled', sum1)
