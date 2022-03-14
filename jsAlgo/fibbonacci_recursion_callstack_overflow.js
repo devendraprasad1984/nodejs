da02116f753b43108edad8dde8ee5cb7a18e85db
devendra prasad <devendraprasad1984@gmail.com>
2022-03-14 10:29:47 +0530

    update

---
+++ jsAlgo/fibbonacci_recursion_callstack_overflow.js
@@ -0,0 +1,13 @@
+let sum = 0
+
+function fib(n) {
+    if (n <= 2) return 1
+    sum = fib(n - 1) + fib(n - 2)
+    return sum
+}
+
+console.log(fib(10)) //55
+// fib(100) //call stack exceeds, node runs recursion on callstack and for such big operation it becomes overflow
+
+//big O will be 2^n -exponential and is very very bad
+
