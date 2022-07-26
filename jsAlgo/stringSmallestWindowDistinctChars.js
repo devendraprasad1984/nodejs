b579c0b8004a6e7b865b6c6e50c6b078aef61933
devendra prasad <devendraprasad1984@gmail.com>
2022-07-26 21:29:04 +0530

    update

---
+++ jsAlgo/stringSmallestWindowDistinctChars.js
@@ -0,0 +1,13 @@
+//Amazon
+//given a string, find the length of smallest window that contains every distinct character
+//chars may appear more than once
+//jiujitsu, should return 5 corresponding to final 5 letters
+
+let dict = {}
+let str1 = 'jiujitsu'
+for (let i of str1) {
+  dict[i] = dict[i] === undefined ? 0 : dict[i] + 1
+}
+let uniqueChars = Object.keys(dict)
+let uniqueCharsLen = uniqueChars.length
+console.log('object', dict, 'unique chars in string', str1, uniqueChars, uniqueCharsLen)
